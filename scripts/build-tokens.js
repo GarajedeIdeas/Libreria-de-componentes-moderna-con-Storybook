b4139203e17369b090f0400e7d62c9b1e8732039
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 14:36:14 +0200

    13 code solved

---
+++ scripts/build-tokens.js
@@ -0,0 +1,48 @@
+const { choices, decisions } = require("../tokens");
+const fs = require("fs");
+
+const toKebabCase = (string) =>
+  string
+    .replace(/([A-Z])([A-Z])/g, "$1-$2")
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/[\s_]+/g, "-")
+    .toLowerCase();
+
+const cleanLines = (string = "") => string.trim().replace(/^\n\n/gm, "\n");
+
+function transformTokens(parentKey, object) {
+  const objectKeys = Object.keys(object);
+
+  return objectKeys.reduce((transformedTokens, objectKey) => {
+    const value = object[objectKey];
+    const customProperty = parentKey
+      ? toKebabCase(`${parentKey}-${objectKey}`)
+      : toKebabCase(`${objectKey}`);
+
+    if (Array.isArray(value)) {
+      return `${transformedTokens}\n  --${customProperty}: ${value.join(
+        ", "
+      )};`;
+    } else if (typeof value === "object") {
+      return `${transformedTokens}\n${transformTokens(customProperty, value)}`;
+    }
+
+    const label = `--${parentKey}-${toKebabCase(objectKey)}`;
+    return `${transformedTokens}\n  ${label}: ${value};`;
+  }, "");
+}
+
+function buildTokens() {
+  const transformedChoices = transformTokens(null, choices);
+  const transformedDecisions = transformTokens(null, decisions);
+  const customProperties = `${transformedChoices}${transformedDecisions}`;
+
+  const data = `:root {\n  ${cleanLines(customProperties)}\n}\n`;
+
+  fs.writeFile("./styles/tokens.css", data, "utf8", (error) => {
+    if (error) throw error;
+    console.log("🎨 Custom properties created!");
+  });
+}
+
+buildTokens();
