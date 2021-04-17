8e6d3106f6b20ee706f34568d667953a4fb59585
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 12:29:58 +0200

    05 code solved

--- atoms/Card/Card.stories.js
+++ atoms/Card/Card.stories.js
@@ -8,6 +8,31 @@ const ListTemplate = ({ items, ...args }) =>
 export default {
   title: "Atoms/Card",
   component: Card,
+  args: {
+    children:
+      "Lacerations coaster sort comings windlance happily EIf-witch handful unbefitting? Decide rising startled Aragorn invitations midnight deserves fortunes innards. You cannot hide. I see you. There is no life in the void. Only death. Mirror Emyn dreamed!",
+  },
+  argTypes: {
+    color: {
+      description: "**options:**",
+      table: {
+        type: {
+          summary: options.colors.map((option) => `'${option}'`).join("|"),
+        },
+      },
+      control: { type: "select", options: options.colors },
+    },
+    size: {
+      description: "**options:**",
+      table: {
+        type: {
+          summary: options.sizes.map((option) => `'${option}'`).join("|"),
+        },
+      },
+      control: { type: "select", options: options.sizes },
+    },
+    children: { control: "text" },
+  },
 };
 
 export const Default = Template.bind({});
@@ -22,8 +47,8 @@ Dragabble.args = {
   isDraggable: true,
 };
 
-export const Colors = ListTemplate.bind({})
-Colors.args = { items: options.colors.map((color) => ({ color })) }
+export const Colors = ListTemplate.bind({});
+Colors.args = { items: options.colors.map((color) => ({ color })) };
 
-export const Sizes = ListTemplate.bind({})
-Sizes.args = { items: options.sizes.map((size) => ({ size })) }
+export const Sizes = ListTemplate.bind({});
+Sizes.args = { items: options.sizes.map((size) => ({ size })) };
