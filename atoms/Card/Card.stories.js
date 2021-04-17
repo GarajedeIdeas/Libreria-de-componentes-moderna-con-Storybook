a0cea5347646d0ea31f77120aacbf40d3ed842bc
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 12:13:11 +0200

    solve 04 code

--- atoms/Card/Card.stories.js
+++ atoms/Card/Card.stories.js
@@ -2,6 +2,8 @@ import Card from ".";
 import { options } from "./constants";
 
 const Template = (args) => <Card {...args} />;
+const ListTemplate = ({ items, ...args }) =>
+  items.map((item, index) => <Card key={index} {...args} {...item} />);
 
 export default {
   title: "Atoms/Card",
@@ -20,8 +22,8 @@ Dragabble.args = {
   isDraggable: true,
 };
 
-export const Colors = () =>
-  options.colors.map((color, index) => <Card key={index} color={color} />);
+export const Colors = ListTemplate.bind({})
+Colors.args = { items: options.colors.map((color) => ({ color })) }
 
-export const Sizes = () =>
-  options.sizes.map((size, index) => <Card key={index} size={size} />);
+export const Sizes = ListTemplate.bind({})
+Sizes.args = { items: options.sizes.map((size) => ({ size })) }
