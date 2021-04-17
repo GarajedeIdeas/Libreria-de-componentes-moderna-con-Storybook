467520e3138c0e5c7f4a757ee65b824766dc89eb
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 14:31:10 +0200

    12 code solved

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,24 +2,24 @@ import "minireset.css";
 import "../styles/globals.css";
 import "../styles/tokens.css";
 
-const styles = {
-  display: "flex",
-  flexDirection: "column",
-  maxHeight: "auto",
-  justifyContent: "flex-start",
-  alignContent: "flex-start",
-  flexWrap: "wrap",
-  height: "100%",
-  gap: "10px 30px",
-};
+const getStyles = ({ __sb } = {}) => ({
+  display: 'flex',
+  flexDirection: __sb?.fd || 'column',
+  maxHeight: __sb?.mh || 'auto',
+  justifyContent: 'flex-start',
+  alignContent: 'flex-start',
+  flexWrap: 'wrap',
+  height: '100%',
+  gap: '10px 30px',
+})
 
 export const decorators = [
-  (Story) => (
-    <div style={styles}>
+  (Story, { parameters }) => (
+    <div style={getStyles(parameters)}>
       <Story />
     </div>
   ),
-];
+]
 
 export const parameters = {
   argTypes: {
