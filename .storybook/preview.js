d40873500ea8ef09d8a8edabae673ac101211853
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 12:43:26 +0200

    06 code solved

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,26 @@
-import 'minireset.css'
-import '../styles/globals.css'
-import '../styles/tokens.css'
+import "minireset.css";
+import "../styles/globals.css";
+import "../styles/tokens.css";
+
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px",
+};
+
+export const decorators = [
+  (Story) => (
+    <div style={styles}>
+      <Story />
+    </div>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
