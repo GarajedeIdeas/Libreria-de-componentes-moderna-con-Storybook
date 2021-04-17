4775d9c6c10bc1391c9d10897c38cde56cab85b8
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 12:08:24 +0200

    03 code solved

--- atoms/Card/Card.stories.js
+++ atoms/Card/Card.stories.js
@@ -1,21 +1,27 @@
 import Card from ".";
 import { options } from "./constants";
 
+const Template = (args) => <Card {...args} />;
+
 export default {
   title: "Atoms/Card",
   component: Card,
 };
 
-export const Default = () => <Card />;
+export const Default = Template.bind({});
 
-export const Clickable = () => <Card isClickable />;
+export const Clickable = Template.bind({});
+Clickable.args = {
+  isClickable: true,
+};
 
-export const Dragabble = () => <Card isDraggable />;
+export const Dragabble = Template.bind({});
+Dragabble.args = {
+  isDraggable: true,
+};
 
-export const Colors = () => options.colors.map((color, index) => (
-  <Card key={index} color={color} />
-));
+export const Colors = () =>
+  options.colors.map((color, index) => <Card key={index} color={color} />);
 
-export const Sizes = () => options.sizes.map((size, index) => (
-  <Card key={index} size={size} />
-));
+export const Sizes = () =>
+  options.sizes.map((size, index) => <Card key={index} size={size} />);
