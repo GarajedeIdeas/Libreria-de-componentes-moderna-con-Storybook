b92929906deda0520d21b643551eb551ca0514dd
Guillermo Rodas <me@guillermorodas.com>
2021-04-17 14:04:24 +0200

    Add untracked file

---
+++ hocs/withStyles.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { getClasses } from '../helpers/styles'
+
+const withStyles = (styles) => (WrappedComponent) => {
+  const WithStylesComponent = (props) => {
+    const allProps = { ...WrappedComponent.defaultProps, ...props }
+    return (
+      <WrappedComponent getStyles={getClasses(styles)(allProps)} {...props} />
+    )
+  }
+
+  WithStylesComponent.displayName = WrappedComponent.displayName
+
+  return WithStylesComponent
+}
+
+export default withStyles
